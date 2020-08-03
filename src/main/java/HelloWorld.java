51dfd4baf4b9a02dc263c07b09c52fca65fa7811
chutimon <chutimon.chaiyawong@allianz.com>
2020-08-03 15:37:09 +0700

    add

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,10 +1,1 @@
-public class HelloWorld {
-    public String sayHi() {
-        return "Hello World";
-    }
-    public String sayHi(String name) {
-        String firstLetter = name.substring(0, 1).toUpperCase();
-        String remainLetter = name.substring(1, name.length());
-        return "Hello " + firstLetter + remainLetter;
-    }
-}
+public class HelloWorld {    public String sayHi() {        return "Hello World";    }    public String sayHi(String name) {        String firstLetter = name.substring(0, 1).toUpperCase();        String remainLetter = name.substring(1);        return "Hello " + firstLetter + remainLetter;    }}
